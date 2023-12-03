41f16f7c3d29b060253b70687d262e68b5603864
Jordan <jkalatchev@hotmail.com>
2023-12-02 22:57:17 -0800

    reformatted config

--- sw/utils/test_api_keys.py
+++ sw/utils/test_api_keys.py
@@ -1,8 +1,6 @@
 import os
 
 import alpaca_trade_api as tradeapi  # type: ignore
-from ta import add_all_ta_features  # type: ignore
-from ta.utils import dropna  # type: ignore
 
 # Verify API's work by checking for expected account id
 api_key = os.environ.get("API_KEY")
@@ -17,19 +15,3 @@ account = api.get_account()
 print(f"Account ID: ${account.id}")
 print(f"Buying Power: ${account.buying_power}")
 print(f"Equity: ${account.equity}")
-
-df = api.get_bars("SPY", tradeapi.TimeFrame.Day, "2021-06-01", "2021-10-01").df
-
-# Clean NaN values
-df = dropna(df)
-
-# Add ta features filling NaN values
-df = add_all_ta_features(
-    df,
-    open="Open",
-    high="High",
-    low="Low",
-    close="Close",
-    volume="Volume_BTC",
-    fillna=True,
-)
