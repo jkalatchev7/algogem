af263849ca6b5849d521028ca8dc2ab311363255
Jordan <jkalatchev@hotmail.com>
2023-12-02 23:03:47 -0800

    Backtest config example

--- strategies/test_config.py
+++ strategies/test_config.py
@@ -4,9 +4,9 @@ with open("example.yml", "r") as file:
     config_data = yaml.safe_load(file)
 print(config_data)
 
-indicators = config_data['strategy']['indicators'].keys()
+indicators = config_data["strategy"]["indicators"].keys()
 
-if 'moving_average' in indicators:
-    print(config_data['strategy']['indicators']['moving_average'])
-if 'rsi' in indicators:
-    print(config_data['strategy']['indicators']['rsi'])
+if "moving_average" in indicators:
+    print(config_data["strategy"]["indicators"]["moving_average"])
+if "rsi" in indicators:
+    print(config_data["strategy"]["indicators"]["rsi"])
